182cd762dcef5a30d622fe5becd76431a190ace4
Andrea Giannantonio <a.giannantonio@gmail.com>
2016-02-23 20:15:21 +0100

    Mi piace fare copia e incolla nelle diverse classi.

--- TennisGame1.php
+++ TennisGame1.php
@@ -2,12 +2,32 @@
 
 class TennisGame1 implements TennisGame
 {
+    protected $player1;
+    protected $player1Points = 0;
+    protected $player2;
+    protected $player2Points = 0;
+
+    public function __construct($player1, $player2)
+    {
+        $this->player1 = $player1;
+        $this->player2 = $player2;
+    }
+
     /**
      * @param  $playerName
      * @return void
      */
     public function wonPoint($playerName)
     {
+        switch ($playerName) {
+            case $this->player1:
+                $this->player1Points++;
+                break;
+            case $this->player2:
+                $this->player2Points++;
+                break;
+
+        }
     }
 
     /**
@@ -15,6 +35,7 @@ class TennisGame1 implements TennisGame
      */
     public function getScore()
     {
+        return Points::$aPoints[$this->player1Points . '-' . $this->player2Points];
     }
 }
 
